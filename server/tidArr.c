53865b65ee323967370ad515bfd95a28f9a4e2c7
T-Keepout <93572860+T-Keepout@users.noreply.github.com>
2025-06-06 10:13:35 +0800

    Delete server directory

--- server/tidArr.c
+++
@@ -1,6 +0,0 @@
-#include "tidArr.h"
-int tidArrInit(tidArr_t *ptidArr, int workerNum){
-    ptidArr->arr = (pthread_t *)calloc(workerNum,sizeof(pthread_t));
-    ptidArr->workerNum = workerNum;
-    return 0;
-}
